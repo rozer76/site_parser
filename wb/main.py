b80d17351a2dce632eb626961305086d6566221b
roz <rozeroz76@gmail.com>
2022-01-15 20:01:35 +0300

    Добавил декорацию ввода

--- wb/main.py
+++ wb/main.py
@@ -2,7 +2,7 @@ from parser import goparse
 
 if __name__ == '__main__':
    wb_parser = goparse()
-   url = input("Введите URL сайта wildberries.ru:")
+   url = input("Привет! Укажи URL сайта wildberries.ru:")
    wb_parser.set_url(url)
    wb_parser.run()
 
